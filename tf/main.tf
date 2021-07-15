38acc30e7f63560a250afff12b4ec11dc1b34bf7
Noel <jwnwilson@hotmail.com.uk>
2021-07-15 08:42:38 +0100

    Updating config

--- tf/main.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-  project     = "flask-example-319821"
-  region      = "europe-west2"
-}
--- tf/modules/docker_repo/main.tf
+++
@@ -1,4 +0,0 @@
-resource "google_container_registry" "registry" {
-  project  = "flask-example"
-  location = "EU"
-}
